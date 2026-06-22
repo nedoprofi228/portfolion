919d0df3894fd80c53e30c8951169ccf984cfd0f
Makar <mvershinin2@gmail.com>
2026-06-22 15:21:55 +0300

    configure git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/portfolion/',
   plugins: [
     vue(),
     tailwindcss(),
